a41c1bd77748f549921ae7d4dd1b9e670b52ef11
Bradley Barrows <bebrws@gmail.com>
2023-03-27 17:37:48 -0700

    more cleanup

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const addon = require(path.join(
 ));
 const { promisify } = require("util");
 
-// console.log(whisper);
-// const whisperAsync = promisify(whisper);
 
 const whisperParams = {
     language: "en",
@@ -17,21 +15,6 @@ const whisperParams = {
     // fname_inp: "./hello.wav",
 };
 
-// const arguments = process.argv.slice(2);
-// const params = Object.fromEntries(
-//     arguments.reduce((pre, item) => {
-//         if (item.startsWith("--")) {
-//             return [...pre, item.slice(2).split("=")];
-//         }
-//         return pre;
-//     }, [])
-// );
-
-// for (const key in params) {
-//     if (whisperParams.hasOwnProperty(key)) {
-//         whisperParams[key] = params[key];
-//     }
-// }
 
 console.log("whisperParams =", whisperParams);
 
@@ -43,51 +26,8 @@ function shouldEnd() {
 }
 
 
-setTimeout(() => { console.log("*****timeout called****"); shouldEndNow = true; }, 10000)
+setTimeout(() => { console.log("*****timeout called****"); shouldEndNow = true; }, 1000000)
 
 addon.startTask(whisperParams, (err, str) => {
     console.log("Value from whisper: ", str);
 }, shouldEnd);
-
-// whisperAsync(whisperParams, ((resp) => {
-//     console.log("resp: ", resp);
-// })).then((result) => {
-//     console.log(`Result from whisper: ${result}`);
-// });
-
-
-// import { SpeechRecorder } from 'speech-recorder';
-
-
-// const params = {
-//     filePath: "test.wav", // required
-//     model: "medium",             // default
-//     output: "JSON",               // default
-// }
-
-
-
-// const recorder = new SpeechRecorder({
-//     onChunkStart: ({ audio }) => {
-//         console.log(Date.now(), "Chunk start");
-//     },
-//     onAudio: ({ speaking, probability, volume }) => {
-//         console.log(Date.now(), speaking, probability, volume);
-//     },
-//     onChunkEnd: () => {
-//         console.log(Date.now(), "Chunk end");
-//     },
-// });
-
-// console.log("Recording for 5 seconds...");
-// recorder.start();
-
-// await delay(2000)
-
-
-// console.log("Done!");
-// recorder.stop();
-
-// const transcript = await whisper(params);
-
-// console.log(transcript);
--- whisper.cpp/examples/addon.node/addon.cpp
+++ whisper.cpp/examples/addon.node/addon.cpp
@@ -25,7 +25,7 @@ struct whisper_params
     int32_t n_threads = std::min(4, (int32_t)std::thread::hardware_concurrency());
 
     int32_t step_ms = 3000;
-    int32_t length_ms = 10000;
+    int32_t length_ms = 1000;
     int32_t keep_ms = 200;
     int32_t capture_id = -1;
     int32_t max_tokens = 32;
