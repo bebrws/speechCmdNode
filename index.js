f5f6fb4485670433a9e81440efbf15d8b6f79d32
Bradley Barrows <bebrws@gmail.com>
2023-03-27 21:36:05 -0700

    Got chat copmletiong owkring

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { Configuration, OpenAIApi } = require("openai")
+
+
 const SegfaultHandler = require('segfault-handler');
 SegfaultHandler.registerHandler('crash.log');
 
@@ -9,25 +12,91 @@ const addon = require(path.join(
 const { promisify } = require("util");
 
 
-const whisperParams = {
-    language: "en",
-    model: path.join(__dirname, "whisper.cpp/models/ggml-base.en.bin"),
-    // fname_inp: "./hello.wav",
+const apiKey = process.env['OPENAI_API_KEY'];
+
+
+let context = [];
+
+const addContext = (text) => {
+    context = [...context, text];
+};
+
+const getContext = () => {
+    return context;
 };
 
 
-console.log("whisperParams =", whisperParams);
+const generateCompletion = async (prompt) => {
 
-let shouldEndNow = false;
-function shouldEnd() {
-    // return true;
-    console.error("Shoudl end was called: ", shouldEndNow)
-    return shouldEndNow;
-}
+    try {
+
+        const configuration = new Configuration({
+            apiKey,
+        });
+
+        const openai = new OpenAIApi(configuration);
 
 
-setTimeout(() => { console.log("*****timeout called****"); shouldEndNow = true; }, 1000000)
+        addContext({ "role": "user", "content": prompt });
+        // addContext({ "role": "assistant", "content": `Read the context, when returning the answer, always placing any code at the very end of the response following the string "###"` });
 
-addon.startTask(whisperParams, (err, str) => {
-    console.log("Value from whisper: ", str);
-}, shouldEnd);
+        console.log(`\n\nMAking request with FULL prompt: ${JSON.stringify(getContext())}\n\n`);
+        const request = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: getContext(),
+        })
+            .then((res) => {
+                console.log(`First rest:`, JSON.stringify(res.data));
+                addContext(res.data.choices[0].message);
+
+                return res.data.choices[0].message;
+            })
+            .catch((err) => {
+                console.error(`Error: ${err}`, JSON.stringify(err));
+            });
+
+        if (request == undefined || !request?.content) {
+            throw new Error("Something went wrong with the request!");
+        }
+
+        return request;
+    } catch (err) {
+        console.error(`"Something went wrong!!": ${err}`, JSON.stringify(err));
+    }
+};
+
+
+
+// const res = await generateCompletion('Write applescript that brings the Google Chrome window to the front and opens up the first tab for Google Meet');
+// console.log(res);
+
+generateCompletion('Write applescript that brings the Google Chrome window to the front and opens up the first tab for Google Meet').then((res) => {
+    console.log(res);
+    generateCompletion('Modify the applescript to mute the Google Meet by sending the keys CMD + D').then((res) => {
+        console.log(res);
+    });
+});
+
+
+// const whisperParams = {
+//     language: "en",
+//     model: path.join(__dirname, "whisper.cpp/models/ggml-base.en.bin"),
+//     // fname_inp: "./hello.wav",
+// };
+
+
+// console.log("whisperParams =", whisperParams);
+
+// let shouldEndNow = false;
+// function shouldEnd() {
+//     // return true;
+//     console.error("Shoudl end was called: ", shouldEndNow)
+//     return shouldEndNow;
+// }
+
+
+// setTimeout(() => { console.log("*****timeout called****"); shouldEndNow = true; }, 1000000)
+
+// addon.startTask(whisperParams, (err, str) => {
+//     console.log("Value from whisper: ", str);
+// }, shouldEnd);
